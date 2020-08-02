b0fd8d94ffbbdef212c3bbcbad4cf729463c6c0a
Ravik27280 <47824408+Ravik27280@users.noreply.github.com>
2020-08-02 16:14:44 +0530

    commit done by Ravi (adding map)

---
+++ eARth(nm377_SIH2020)/Assets/Infinity Code/Online maps/Examples (API usage)/CustomDownloadTileExample.cs
@@ -0,0 +1,55 @@
+﻿/*     INFINITY CODE 2013-2019      */
+/*   http://www.infinity-code.com   */
+
+using UnityEngine;
+
+namespace InfinityCode.OnlineMapsExamples
+{
+    /// <summary>
+    /// Example of interception requests to download tiles
+    /// </summary>
+    [AddComponentMenu("Infinity Code/Online Maps/Examples (API Usage)/CustomDownloadTileExample")]
+    public class CustomDownloadTileExample : MonoBehaviour
+    {
+        private OnlineMaps map;
+
+        private void Start()
+        {
+            map = OnlineMaps.instance;
+
+            // Subscribe to the tile download event.
+            OnlineMapsTileManager.OnStartDownloadTile += OnStartDownloadTile;
+        }
+
+        private void OnStartDownloadTile(OnlineMapsTile tile)
+        {
+            Texture2D tileTexture = new Texture2D(256, 256);
+
+            // Here your code to load tile texture from any source.
+
+            // Apply your texture in the buffer and redraws the map.
+            if (map.control.resultIsTexture)
+            {
+                // Apply tile texture
+                (tile as OnlineMapsRasterTile).ApplyTexture(tileTexture as Texture2D);
+
+                // Send tile to buffer
+                map.buffer.ApplyTile(tile);
+
+                // Destroy the texture, because it is no longer needed.
+                OnlineMapsUtils.Destroy(tileTexture);
+            }
+            else
+            {
+                // Send tile texture
+                tile.texture = tileTexture;
+
+                // Change tile status
+                tile.status = OnlineMapsTileStatus.loaded;
+            }
+
+            // Redraw map (using best redraw type)
+            map.Redraw();
+        }
+    }
+}
