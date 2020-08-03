cc706d8d749a7c6e1689b700b50ad2911b7a5c32
Ravik27280 <47824408+Ravik27280@users.noreply.github.com>
2020-08-03 14:11:58 +0530

    zoom scrit

---
+++ eARth(nm377_SIH2020)/Assets/test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    public GameObject map;
+    private OnlineMaps omScript;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "";
+        omScript = map.GetComponent<OnlineMaps>();
+    }
+
+    public void Update()
+    {
+        Debug.Log(Vector3.Magnitude(map.transform.position - Camera.main.transform.position));
+        omScript.floatZoom = Vector3.Magnitude(map.transform.position - Camera.main.transform.position);
+        Mathf.Clamp(omScript.floatZoom, 2f, 20f);
+    }
+
+    public void ChangetoOpenStreet()
+    {
+        //testing purpose
+        omScript.floatZoom += Time.deltaTime;
+    }
+}
